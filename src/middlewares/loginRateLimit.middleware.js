1e506a40ca490f95cfa958be8961241449e50d70
Miguel Angel Saravia <msaravia@ia.com.mx>
2026-04-23 23:51:24 -0400

    add more tryouts

--- src/middlewares/loginRateLimit.middleware.js
+++ src/middlewares/loginRateLimit.middleware.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5,
+  max: 20,
   skipSuccessfulRequests: true,
   standardHeaders: true,
   legacyHeaders: false,
